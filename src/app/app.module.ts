abe1430f2897e069a911cb1621dedb38e2e70e21
Marek Gabiński <mgabinski@apreel.com>
2024-07-17 20:47:17 +0200

    add translations, bootstrap and some features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -13,10 +19,21 @@ import { TodoListComponent } from './todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+}
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,31 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent {
   newTask: string = '';
   tasks: string[] = [];
   errorMessage: string = '';
+  readonly maxTasks: number = 10;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(private translate: TranslateService) { }
 
   addTask(): void {
-    if (this.newTask.trim().length === 0) {
-      this.errorMessage = 'Task cannot be empty';
+    if (this.isTaskInvalid()) {
       return;
     }
+
     this.tasks.push(this.newTask);
-    this.newTask = '';
-    this.errorMessage = '';
+    this.resetNewTask();
   }
 
   deleteTask(index: number): void {
     this.tasks.splice(index, 1);
+    if (this.tasks.length < this.maxTasks) {
+      this.clearError();
+    }
+  }
+
+  private isTaskInvalid(): boolean {
+    if (this.tasks.length >= this.maxTasks) {
+      this.errorMessage = this.translate.instant('ERROR.MAX_TASKS', { maxTasks: this.maxTasks });
+      return true;
+    }
+
+    if (!this.newTask.trim()) {
+      this.errorMessage = this.translate.instant('ERROR.EMPTY_TASK');
+      return true;
+    }
+
+    return false;
+  }
+
+  private resetNewTask(): void {
+    this.newTask = '';
+    this.clearError();
+  }
+
+  private clearError(): void {
+    this.errorMessage = '';
   }
 }
