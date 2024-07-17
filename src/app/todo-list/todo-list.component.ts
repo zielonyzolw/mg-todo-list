0f7c8b7913e4118342cb652fcffad1b0cddf0643
Marek Gabiński <mgabinski@apreel.com>
2024-07-17 20:52:36 +0200

    add comments

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -7,22 +7,28 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
+  // Create variables
   newTask: string = '';
   tasks: string[] = [];
   errorMessage: string = '';
   readonly maxTasks: number = 10;
 
+  //add translateService
   constructor(private translate: TranslateService) { }
 
+  // create AddTask Method
   addTask(): void {
+    // Check if task is valid
     if (this.isTaskInvalid()) {
       return;
     }
 
+    // Add new task
     this.tasks.push(this.newTask);
     this.resetNewTask();
   }
 
+  // create deleteTask Method
   deleteTask(index: number): void {
     this.tasks.splice(index, 1);
     if (this.tasks.length < this.maxTasks) {
@@ -30,25 +36,31 @@ export class TodoListComponent {
     }
   }
 
+  // create validation error method
   private isTaskInvalid(): boolean {
-    if (this.tasks.length >= this.maxTasks) {
-      this.errorMessage = this.translate.instant('ERROR.MAX_TASKS', { maxTasks: this.maxTasks });
+
+    // add error if task input is empty
+    if (!this.newTask.trim()) {
+      this.errorMessage = this.translate.instant('ERROR.EMPTY_TASK');
       return true;
     }
 
-    if (!this.newTask.trim()) {
-      this.errorMessage = this.translate.instant('ERROR.EMPTY_TASK');
+    // add additional error - if maxTasks = 10
+    if (this.tasks.length >= this.maxTasks) {
+      this.errorMessage = this.translate.instant('ERROR.MAX_TASKS', { maxTasks: this.maxTasks });
       return true;
     }
 
     return false;
   }
 
+  // reset new task input and claer error
   private resetNewTask(): void {
     this.newTask = '';
     this.clearError();
   }
 
+  // clear error message
   private clearError(): void {
     this.errorMessage = '';
   }
